3c7230e83a594789bd7d6e0e89fcee9b44669288
fjh1997 <549308442@qq.com>
2020-07-02 18:41:41 +0800

    Update backup_docker_volume.sh

--- backup_docker_volume.sh
+++ backup_docker_volume.sh
@@ -1,1 +1,2 @@
-sudo docker run --rm   --volume $1:/dbdata   --volume $(pwd):/backup   ubuntu   tar cvf /backup/$2 /dbdata
+#!/bin/bash
+docker run --rm   --volume $1:/dbdata   --volume $(pwd):/backup   ubuntu   tar cvf /backup/$2 /dbdata
