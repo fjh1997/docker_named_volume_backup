34ab82f6432888ee82616373581aede4810c85fb
fjh1997 <549308442@qq.com>
2020-07-02 18:42:08 +0800

    Update resore_docker_volume.sh

--- resore_docker_volume.sh
+++ resore_docker_volume.sh
@@ -1,5 +1,2 @@
-docker run --rm \ 
-  --volume $1:/dbdata \
-  --volume $(pwd):/backup\
-  ubuntu \
-  tar xvf /backup/$2 -C /dbdata --strip 1
+#!/bin/bash
+docker run --rm  --volume $1:/dbdata --volume $(pwd):/backup ubuntu  tar xvf /backup/$2 -C /dbdata --strip 1
