92cec1f2e2d52f75fd2243bb52600e6a755e818e
继晗 傅 <549308442@qq.com>
2020-07-02 18:31:47 +0800

    script

---
+++ backup_docker_volume.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm   --volume $1:/dbdata   --volume $(pwd):/backup   ubuntu   tar cvf /backup/$2 /dbdata
---
+++ resore_docker_volume.sh
@@ -0,0 +1,5 @@
+docker run --rm \ 
+  --volume $1:/dbdata \
+  --volume $(pwd):/backup\
+  ubuntu \
+  tar xvf /backup/$2 -C /dbdata --strip 1
